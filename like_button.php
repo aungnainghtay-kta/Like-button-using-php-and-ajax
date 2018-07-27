754c50baf8d732b61d320803774c961a037eb2ee
Aung Naing Htay <aungnainghtay.main@gmail.com>
2018-07-27 10:32:13 +0630

    Added like button php file

---
+++ like_button.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Like Button</title>
+<link rel ="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+<div class="container">
+	<div class="well">
+		<h1 class="text-center">Like Button</h1>
+		<hr>
+		<button class="btn btn-primary">Like Button</button>
+		<div id="show"><h3>100 likes</h3></div>
+</div>
+</div>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
